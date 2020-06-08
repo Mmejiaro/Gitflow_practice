912c0a2068f9e8ab388cf11824f320f7203d8399
mmejiaro <mmejiaro@uncc.edu>
2020-06-08 11:21:52 -0400

    passed QA

--- Test/src/Hello_World.java
+++ Test/src/Hello_World.java
@@ -4,6 +4,8 @@ public class Hello_World {
 	public static void main(String[] args) {
 		System.out.print("Hello World");
 		// release branch created sent to QA for testing
+		
+		// passed QA preparing for merge with develop and master
 	}
 
 }
